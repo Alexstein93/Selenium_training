6c4954b77876793b57c1f0d496dfaae7930de1c3
Alex <alxshedeoz@gmail.com>
2019-03-05 15:02:08 -0600

    Update TC_01_Validate_Login.java
    Add some info about the methods

--- src_page/TestCases/TC_01_Validate_Login.java
+++ src_page/TestCases/TC_01_Validate_Login.java
@@ -11,17 +11,22 @@ import Base.BaseDriver;
 
 public class TC_01_Validate_Login extends BaseDriver {
 
+	//Obtain and store the test case name in a variable
 	private String testCaseName = this.getClass().getSimpleName();
 	Login obj = null;
-	//GoogleSearch searchPAge = null;
 	
-
+	/*
+	 * Method beforeClass set the driver for the browser and the test case name
+	 */
 	@Before
 	public void beforeTest() throws InterruptedException, IOException  {
 		beforeClass(configData.get("Browser"), testCaseName);
 		obj= new Login(driver);
 	}
 	
+	/*
+	 * Call to the steps or our test case, they are in the Login class
+	 */
 	@Test
 	public void testMain()  {
 		
@@ -34,6 +39,9 @@ public class TC_01_Validate_Login extends BaseDriver {
 		
 	}
 	
+	/*
+	 * Method afterClass quit the browser and print the test case name
+	 */
 	@After
 	public void tearDown() throws InterruptedException{
 		afterClass(testCaseName);
